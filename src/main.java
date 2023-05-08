ce003f8a89f194de94e28333a5386dc47b9852fb
SV Developer <azimazizov9150@gmail.com>
2023-05-08 11:34:31 +0400

    Added caller ID

---
+++ src/main.java
@@ -0,0 +1,9 @@
+package me.svdev.main;
+
+class Main {
+    public static void main() {
+        System.out.println("Hello world!");
+        var callerId = "Ingress Academy";
+        System.out.println("It's " + callerId);
+    }
+}
